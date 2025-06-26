cf06724bec35e4e3f4ec452b64e4329f00494562
resAsus <respectedbro@yandex.ru>
2025-06-26 10:48:45 +0300

    test

--- main.js
+++ main.js
@@ -69,46 +69,105 @@ const showData = () => {
   }
 };
 
-
 todoBtn.addEventListener('click', () => {
   addTask();
 });
 
+
+const toggleTasks = (task) => {
+    task.classList.toggle('checked')
+}
+
+const removeTask = (task) => {
+    task.remove()
+}
+
+const setReminder = (reminderEl, task) => {
+    if (task.classList.contains('checked')) {
+        alert('Задача уже выполнена');
+        return;
+    }
+
+    const sec = prompt('Введите время в секундах: ')
+
+    if (!sec || isNaN(sec)) {
+        alert('Некорректный ввод')
+    } else {
+        const timeInSec = parseInt(sec) * 1000;
+
+        reminderEl.innerHTML = '&#9742;';
+        reminderEl.classList.add('remind');
+
+        setTimeout(() => {
+            reminderEl.innerHTML = '&#9743;';
+            reminderEl.classList.remove('remind');
+            saveData();
+            alert(`Напоминание о задаче - ${task.firstChild.textContent}`);
+        }, timeInSec);
+    }
+
+    saveData();
+}
+
 todoList.addEventListener('click', (e) => {
-  if (e.target.tagName === 'LI') {
-    e.target.classList.toggle('checked');
-    const remindBtn = e.target.querySelector('div');
-    if (remindBtn) {
-      remindBtn.classList.toggle('hidden', e.target.classList.contains('checked'));
+    const target = e.target
+    const taskItem = target.closest('li')
+
+    if (!taskItem) return
+
+
+    switch (target.tagName) {
+        case 'LI':
+            toggleTasks(taskItem);
+            break
+        case 'SPAN':
+            removeTask(taskItem)
+            break
+        case 'DIV':
+            setReminder(target, taskItem)
+            break
     }
     saveData();
-  } else if (e.target.tagName === 'SPAN') {
-    e.target.parentElement.remove();
-    saveData();
-  } else if (e.target.tagName === 'DIV') {
-    if (e.target.closest('li').classList.contains('checked')) return;
+})
 
-    const sec = prompt('Введите время напоминания в секундах:');
-    if (sec && !isNaN(sec)) {
-      const time = parseInt(sec) * 1000;
-      const taskItem = e.target.closest('li');
+// todoList.addEventListener('click', (e) => {
+//   if (e.target.tagName === 'LI') {
+//     e.target.classList.toggle('checked');
+//     const remindBtn = e.target.querySelector('div');
+//     if (remindBtn) {
+//       remindBtn.classList.toggle('hidden', e.target.classList.contains('checked'));
+//     }
+//     saveData();
+//   } else if (e.target.tagName === 'SPAN') {
+//     e.target.parentElement.remove();
+//     saveData();
+//   } else if (e.target.tagName === 'DIV') {
+//     if (e.target.closest('li').classList.contains('checked')) return;
+//
+//     const sec = prompt('Введите время напоминания в секундах:');
+//     if (sec && !isNaN(sec)) {
+//       const time = parseInt(sec) * 1000;
+//       const taskItem = e.target.closest('li');
+//
+//       e.target.innerHTML = '&#9742;';
+//       e.target.classList.add('remind');
+//
+//       setTimeout(() => {
+//         if (!taskItem.classList.contains('checked')) {
+//           e.target.innerHTML = '&#9743;';
+//           e.target.classList.remove('remind');
+//           saveData();
+//           alert(`Напоминание о задаче - ${taskItem.firstChild.textContent}`);
+//         }
+//       }, time);
+//
+//       saveData();
+//     }
+//   }
+// });
 
-      e.target.innerHTML = '&#9742;';
-      e.target.classList.add('remind');
 
-      setTimeout(() => {
-        if (!taskItem.classList.contains('checked')) {
-          e.target.innerHTML = '&#9743;';
-          e.target.classList.remove('remind');
-          saveData();
-          alert(`Напоминание о задаче - ${taskItem.firstChild.textContent}`);
-        }
-      }, time);
 
-      saveData();
-    }
-  }
-});
 showData();
 
 setInterval(updateGradient, 20);
