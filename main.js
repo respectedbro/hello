b37783821fdb0f5bdfb7f158b920350ea26a3971
resAsus <respectedbro@yandex.ru>
2025-06-26 14:14:50 +0300

    dn

--- main.js
+++ main.js
@@ -54,6 +54,15 @@ const addTask = () => {
   saveData();
 };
 
+const filter = (clickedButton) => {
+
+    filterButtons.forEach(btn => {
+        btn.classList.remove('pick');
+    });
+
+    clickedButton.classList.add('pick');
+}
+
 const saveData = () => {
   localStorage.setItem('data', todoList.innerHTML);
 };
@@ -69,49 +78,63 @@ const showData = () => {
   }
 };
 
+const toggleTasks = (task) => {
+    task.classList.toggle('checked')
+}
+
+const removeTask = (task) => {
+    task.remove()
+}
+
+const setReminder = (reminderEl, task) => {
+    if (task.classList.contains('checked')) {
+        alert('Задача выполнена');
+        return;
+    }
+
+    const sec = prompt('Введите время в секундах: ')
+
+    if (!sec || isNaN(sec)) {
+        alert('Некорректный ввод')
+    } else {
+        const timeInSec = parseInt(sec) * 1000;
+
+        reminderEl.innerHTML = '&#9742;';
+        reminderEl.classList.add('remind');
+
+        setTimeout(() => {
+            reminderEl.innerHTML = '&#9743;';
+            reminderEl.classList.remove('remind');
+            saveData();
+            alert(`Напоминание о задаче - ${task.firstChild.textContent}`);
+        }, timeInSec);
+        saveData();
+    }
+}
 
 todoBtn.addEventListener('click', () => {
-  addTask();
+    addTask();
 });
-
 todoList.addEventListener('click', (e) => {
-  if (e.target.tagName === 'LI') {
-    e.target.classList.toggle('checked');
-    const remindBtn = e.target.querySelector('div');
-    if (remindBtn) {
-      remindBtn.classList.toggle('hidden', e.target.classList.contains('checked'));
+    const target = e.target
+    const taskItem = target.closest('li')
+
+    if (!taskItem) return
+
+
+    switch (target.tagName) {
+        case 'LI':
+            toggleTasks(taskItem);
+            break
+        case 'SPAN':
+            removeTask(taskItem)
+            break
+        case 'DIV':
+            setReminder(target, taskItem)
+            break
     }
     saveData();
-  } else if (e.target.tagName === 'SPAN') {
-    e.target.parentElement.remove();
-    saveData();
-  } else if (e.target.tagName === 'DIV') {
-    if (e.target.closest('li').classList.contains('checked')) return;
-
-    const sec = prompt('Введите время напоминания в секундах:');
-    if (sec && !isNaN(sec)) {
-      const time = parseInt(sec) * 1000;
-      const taskItem = e.target.closest('li');
-
-      e.target.innerHTML = '&#9742;';
-      e.target.classList.add('remind');
-
-      setTimeout(() => {
-        if (!taskItem.classList.contains('checked')) {
-          e.target.innerHTML = '&#9743;';
-          e.target.classList.remove('remind');
-          saveData();
-          alert(`Напоминание о задаче - ${taskItem.firstChild.textContent}`);
-        }
-      }, time);
-
-      saveData();
-    }
-  }
-});
-showData();
-
-setInterval(updateGradient, 20);
+})
 
 todoFilter.addEventListener('click', (e) => {
   if (e.target.classList.contains('btn-filters')) {
@@ -129,43 +152,9 @@ todoFilter.addEventListener('click', (e) => {
   }
 });
 
-// todoFilter.addEventListener('click', (e) => {
-//   if (e.target.classList.contains('btn-filters')) {
-//     filter(e.target);
-//
-//     const tasks = document.querySelectorAll('.todo__list li');
-//
-//     if (e.target.classList.contains('todo__all-tasks')) {
-//       // Показать все задачи
-//       tasks.forEach(task => {
-//         task.style.display = 'flex';
-//       });
-//     } else if (e.target.classList.contains('todo__done-tasks')) {
-//       tasks.forEach(task => {
-//         if (task.classList.contains('checked')) {
-//           task.style.display = 'flex';
-//         } else {
-//           task.style.display = 'none';
-//         }
-//       });
-//     } else if (e.target.classList.contains('todo__not-done-tasks')) {
-//       tasks.forEach(task => {
-//         if (!task.classList.contains('checked')) {
-//           task.style.display = 'flex';
-//         } else {
-//           task.style.display = 'none';
-//         }
-//       });
-//     }
-//   }
-// });
+showData();
 
-const filter = (clickedButton) => {
+setInterval(updateGradient, 20);
 
-  filterButtons.forEach(btn => {
-    btn.classList.remove('pick');
-  });
 
-  clickedButton.classList.add('pick');
-}
 
